3742c9325df16fbfab3f84412972b291bc34d83f
Aurèle <aurele.nicolas@epitech.eu>
2022-02-22 00:21:03 +0100

    Added some exceptions (#11)

--- src/Exception.hpp
+++ src/Exception.hpp
@@ -8,15 +8,16 @@
 #ifndef EXCEPTION_HPP_
 #define EXCEPTION_HPP_
 
-#define yeet(message) throw new nts::Exception(message)
+#define yeet(message) throw new nts::NtsException::YeetException(message)
 
 #include <exception>
 #include <string>
 
+
 namespace nts {
-    class Exception : std::exception {
+    class NtsException : std::exception {
         public:
-            Exception(std::string const &message);
+            NtsException(std::string const &message);
             const char *what() const noexcept;
 
         protected:
@@ -24,6 +25,26 @@ namespace nts {
 
         private:
     };
+}
+
+namespace nts {
+    class YeetException : NtsException { const char *what() const noexcept { return "yeet"; }};
+    class ParserException : NtsException {};
+    class InvalidReadException : ParserException {};
+    class InvalidSyntaxException : InvalidReadException {};
+    class RuntimeException : NtsException {};
+    class ComputeException : RuntimeException {};
+    class SimulateException : RuntimeException {};
+    class InvalidPinException : RuntimeException {};
+    class InvalidTypeException : RuntimeException {};
+    class InvalidValueException : RuntimeException {};
+    class InvalidLinkException : RuntimeException {};
+    class InvalidComponentException : RuntimeException {};
+    class InvalidCircuitException : RuntimeException {};
+    class InvalidFileException : RuntimeException {};
+    class InvalidComponentNameException : InvalidComponentException {};
+    class RickException : YeetException { const char *what() const noexcept { return "Never gonna give you up, never gonna let you down, never gonna run around and desert you. Never gonna make you cry, never gonna say goodbye. Never gonna tell a lie and hurt you."; }};
+    class AAAAAAAAAAAAAAAAAAAAAAAAException : YeetException { const char *what() const noexcept { return "AAAAAAAAAAAAAAAAAAAAAAAA"; }};
 }
 
 #endif /* !EXCEPTION_HPP_ */
