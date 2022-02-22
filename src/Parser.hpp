3720cb5c793ce806fbcf3454960b5cc94b74389b
Aurèle <aurele.nicolas@epitech.eu>
2022-02-22 01:26:47 +0100

    moved component creation from parser to factory

--- src/Parser.hpp
+++ src/Parser.hpp
@@ -18,22 +18,6 @@ namespace nts {
             Parser();
             ~Parser();
             void fillCircuit(const std::string&, Circuit&);
-            std::unique_ptr<nts::IComponent> createComponent(const std::string& type);
-        protected:
-        private:
-            [[nodiscard]]std::unique_ptr<nts::IComponent> create4001(const std::string &name) const;
-            [[nodiscard]]std::unique_ptr<nts::IComponent> create4008(const std::string &name) const;
-            [[nodiscard]]std::unique_ptr<nts::IComponent> create4011(const std::string &name) const;
-            [[nodiscard]]std::unique_ptr<nts::IComponent> create4013(const std::string &name) const;
-            [[nodiscard]]std::unique_ptr<nts::IComponent> create4017(const std::string &name) const;
-            [[nodiscard]]std::unique_ptr<nts::IComponent> create4030(const std::string &name) const;
-            [[nodiscard]]std::unique_ptr<nts::IComponent> create4040(const std::string &name) const;
-            [[nodiscard]]std::unique_ptr<nts::IComponent> create4069(const std::string &name) const;
-            [[nodiscard]]std::unique_ptr<nts::IComponent> create4071(const std::string &name) const;
-            [[nodiscard]]std::unique_ptr<nts::IComponent> create4081(const std::string &name) const;
-            [[nodiscard]]std::unique_ptr<nts::IComponent> create4094(const std::string &name) const;
-            [[nodiscard]]std::unique_ptr<nts::IComponent> create4514(const std::string &name) const;
-            [[nodiscard]]std::unique_ptr<nts::IComponent> create4801(const std::string &name) const;
     };
 }
 
