11e5a99dbabf6e71e22ec6050edbc33c5fbda935
Cavonstavant <constant.vigneron@protonmail.com>
2022-02-22 18:06:34 +0100

    [+] created a header file for basic_gates.cpp and added norGate

---
+++ src/basic_gates/basic_gates.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2022
+** nanotekspice
+** File description:
+** basic_gates
+*/
+
+#ifndef NANOTEKSPICE_BASIC_GATES_HPP
+#define NANOTEKSPICE_BASIC_GATES_HPP
+
+#include "../IComponent.hpp"
+
+
+namespace nts {
+    Tristate andGate(Tristate a, Tristate b);
+    Tristate orGate(Tristate a, Tristate b);
+    Tristate xorGate(Tristate a, Tristate b);
+    Tristate notGate(Tristate a);
+    Tristate nandGate(Tristate a, Tristate b);
+    Tristate norGate(Tristate a, Tristate b);
+}
+
+
+#endif //NANOTEKSPICE_BASIC_GATES_HPP
