5572f1e25a0faeac01d35977d22e50f644c214c5
Restyled.io <commits@restyled.io>
2022-02-26 19:03:29 +0000

    Restyled by clang-format

--- src/AComponent.cpp
+++ src/AComponent.cpp
@@ -9,23 +9,16 @@
 #include "Exception.hpp"
 #include <iostream>
 
+nts::AComponent::AComponent(const std::string &name) { this->_name = name; }
 
-nts::AComponent::AComponent(const std::string& name)
-{
-    this->_name = name;
-}
+void nts::AComponent::setName(const std::string &name) { this->_name = name; }
 
-void nts::AComponent::setName(const std::string &name) {
-    this->_name = name;
-}
-
-std::string nts::AComponent::getName() const {
-    return (this->_name);
-}
+std::string nts::AComponent::getName() const { return (this->_name); }
 
 bool nts::AComponent::operator==(const nts::IComponent &other) const {
-    auto &other_component = dynamic_cast<AComponent &>(const_cast<nts::IComponent&>(other));
-    return (this->_name == other_component.getName());
+  auto &other_component =
+      dynamic_cast<AComponent &>(const_cast<nts::IComponent &>(other));
+  return (this->_name == other_component.getName());
 }
 
 /**
@@ -34,21 +27,21 @@ bool nts::AComponent::operator==(const nts::IComponent &other) const {
  * @param other
  * @param otherPin
  */
-void nts::AComponent::setLink(std::size_t pin, nts::IComponent &other, std::size_t otherPin)
-{
-    LinkPair newPair = LinkPair(pin, otherPin, other);
+void nts::AComponent::setLink(std::size_t pin, nts::IComponent &other,
+                              std::size_t otherPin) {
+  LinkPair newPair = LinkPair(pin, otherPin, other);
 
-    _links.emplace_back(newPair);
-    _states.emplace_back(std::make_pair(pin, nts::Tristate::UNDEFINED));
-    other.setPin(otherPin, *this, pin);
+  _links.emplace_back(newPair);
+  _states.emplace_back(std::make_pair(pin, nts::Tristate::UNDEFINED));
+  other.setPin(otherPin, *this, pin);
 }
 
-void nts::AComponent::setPin(std::size_t pin, nts::IComponent &other, std::size_t otherPin)
-{
-    LinkPair newPair = LinkPair(pin, otherPin, other);
+void nts::AComponent::setPin(std::size_t pin, nts::IComponent &other,
+                             std::size_t otherPin) {
+  LinkPair newPair = LinkPair(pin, otherPin, other);
 
-    _links.emplace_back(newPair);
-    _states.emplace_back(std::make_pair(pin, nts::Tristate::UNDEFINED));
+  _links.emplace_back(newPair);
+  _states.emplace_back(std::make_pair(pin, nts::Tristate::UNDEFINED));
 }
 
 /**
@@ -57,13 +50,13 @@ void nts::AComponent::setPin(std::size_t pin, nts::IComponent &other, std::size_
  * @return the state of the pin
  * @throw nts::Exception::UnknownPinException if the pin is not found
  */
-nts::Tristate nts::AComponent::pollState(size_t pin) const
-{
-    for (auto &state : _states) {
-        if (std::get<size_t>(state) == pin)
-            return (std::get<Tristate>(state));
-    }
-    throw nts::Exception::UnknownPinException(std::string("pin: " + std::to_string(pin) + " not found"), pin);
+nts::Tristate nts::AComponent::pollState(size_t pin) const {
+  for (auto &state : _states) {
+    if (std::get<size_t>(state) == pin)
+      return (std::get<Tristate>(state));
+  }
+  throw nts::Exception::UnknownPinException(
+      std::string("pin: " + std::to_string(pin) + " not found"), pin);
 }
 
 /**
@@ -72,51 +65,56 @@ nts::Tristate nts::AComponent::pollState(size_t pin) const
  * @param newState
  * @throw nts::Exception::UnknownPinException if the pin is not found
  */
-void nts::AComponent::setState(size_t pin, Tristate newState)
-{
-    for (auto &state : _states) {
-        if (std::get<size_t>(state) == pin) {
-            state = std::make_pair(pin, newState);
-            return;
-        }
+void nts::AComponent::setState(size_t pin, Tristate newState) {
+  for (auto &state : _states) {
+    if (std::get<size_t>(state) == pin) {
+      state = std::make_pair(pin, newState);
+      return;
     }
-    throw nts::Exception::UnknownPinException(std::string("pin: " + std::to_string(pin) + " not found"), pin);
+  }
+  throw nts::Exception::UnknownPinException(
+      std::string("pin: " + std::to_string(pin) + " not found"), pin);
 }
 
 /**
  * @brief display infos about the component
  */
 void nts::AComponent::dump() const {
-    std::cout << _name << ": {" << std::endl;
-    for (auto &it : _links)
-        std::cout << "\t linked to " << it.get().component.getName() << "from pin: " << it.get().pin_other << " to pin: " << it.get().pin_self << std::endl;
-    std::cout << "}" << std::endl;
+  std::cout << _name << ": {" << std::endl;
+  for (auto &it : _links)
+    std::cout << "\t linked to " << it.get().component.getName()
+              << "from pin: " << it.get().pin_other
+              << " to pin: " << it.get().pin_self << std::endl;
+  std::cout << "}" << std::endl;
 }
 
 nts::Tristate nts::AComponent::getState(size_t pin) const {
-    for (auto &state : _states) {
-        if (std::get<size_t>(state) == pin)
-            return (std::get<Tristate>(state));
-    }
-    throw nts::Exception::UnknownPinException(std::string("pin: " + std::to_string(pin) + " not found"), pin);
+  for (auto &state : _states) {
+    if (std::get<size_t>(state) == pin)
+      return (std::get<Tristate>(state));
+  }
+  throw nts::Exception::UnknownPinException(
+      std::string("pin: " + std::to_string(pin) + " not found"), pin);
 }
 
 /**
- * @brief Can't simulate an abstract component (should be implemented in the child class)
+ * @brief Can't simulate an abstract component (should be implemented in the
+ * child class)
  * @param tick
  * @throw nts::Exception::InvalidComponentException
  */
-void nts::AComponent::simulate(std::size_t tick)
-{
-    throw nts::Exception::InvalidComponentException("simulate is not implemented");
+void nts::AComponent::simulate(std::size_t tick) {
+  throw nts::Exception::InvalidComponentException(
+      "simulate is not implemented");
 }
 
 /**
- * @brief Can't compute an abstract component (should be implemented in the child class)
+ * @brief Can't compute an abstract component (should be implemented in the
+ * child class)
  * @param pin
  * @return the state of the pin
  * @throw nts::Exception::InvalidComponentException
  */
 nts::Tristate nts::AComponent::compute(std::size_t pin) {
-    throw nts::Exception::InvalidComponentException("compute is not implemented");
+  throw nts::Exception::InvalidComponentException("compute is not implemented");
 }
--- src/AComponent.hpp
+++ src/AComponent.hpp
@@ -9,49 +9,53 @@
 #define NANOTEKSPICE_ACOMPONENT_HPP
 
 #include "IComponent.hpp"
-#include <map>
-#include <vector>
-#include <string>
-#include <memory>
 #include <functional>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
 
-namespace nts
-{
+namespace nts {
 struct LinkPair {
-    LinkPair() = delete;
-    LinkPair(size_t pin_self, size_t pin_other, IComponent& component) : component(component) {};
-    ~LinkPair() = default;
-    size_t pin_self{};
-    size_t pin_other{};
-    IComponent& component;
+  LinkPair() = delete;
+  LinkPair(size_t pin_self, size_t pin_other, IComponent &component)
+      : component(component){};
+  ~LinkPair() = default;
+  size_t pin_self{};
+  size_t pin_other{};
+  IComponent &component;
 };
 class AComponent : public IComponent {
 public:
-    AComponent() = default;
-    ~AComponent() override = default;
-    explicit AComponent(const std::string& name);
-    AComponent(const AComponent &other) = delete;
-    AComponent &operator=(const AComponent &other) = delete;
-    void simulate(std::size_t tick) override;
-    nts::Tristate compute(std::size_t pin) override;
-    bool operator==(const IComponent &other) const override;
-    inline bool operator!=(const IComponent &other) const override  {
-        return !(*this == other);
-    }
-    void dump() const override;
-    void setLink(std::size_t pin, nts::IComponent &other, std::size_t otherPin) override;
-    void setPin(std::size_t pin, nts::IComponent &other, std::size_t otherPin) override;
-    void setName(const std::string &name) override;
-    [[nodiscard]] std::string getName() const override;
-    [[nodiscard]] Tristate pollState(size_t pin) const override;
-    void setState(size_t pin, Tristate newState) override;
-    [[nodiscard]] Tristate getState(size_t pin) const;
+  AComponent() = default;
+  ~AComponent() override = default;
+  explicit AComponent(const std::string &name);
+  AComponent(const AComponent &other) = delete;
+  AComponent &operator=(const AComponent &other) = delete;
+  void simulate(std::size_t tick) override;
+  nts::Tristate compute(std::size_t pin) override;
+  bool operator==(const IComponent &other) const override;
+  inline bool operator!=(const IComponent &other) const override {
+    return !(*this == other);
+  }
+  void dump() const override;
+  void setLink(std::size_t pin, nts::IComponent &other,
+               std::size_t otherPin) override;
+  void setPin(std::size_t pin, nts::IComponent &other,
+              std::size_t otherPin) override;
+  void setName(const std::string &name) override;
+  [[nodiscard]] std::string getName() const override;
+  [[nodiscard]] Tristate pollState(size_t pin) const override;
+  void setState(size_t pin, Tristate newState) override;
+  [[nodiscard]] Tristate getState(size_t pin) const;
+
 protected:
-    std::string _name;
-    std::vector<std::reference_wrapper<LinkPair>> _links;
-    std::vector<std::pair<std::size_t, Tristate>> _states;
+  std::string _name;
+  std::vector<std::reference_wrapper<LinkPair>> _links;
+  std::vector<std::pair<std::size_t, Tristate>> _states;
+
 private:
 };
-}
+} // namespace nts
 
 #endif /* !NANOTEKSPICE_ACOMPONENT_HPP */
