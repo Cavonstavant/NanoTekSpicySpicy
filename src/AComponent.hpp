94dc1bfa79b3c08605adebd59a5115e7034b46dc
Cavonstavant <constant.vigneron@protonmail.com>
2022-02-26 20:00:29 +0100

    [+] implement Acomponent simulate, compute and operator==

--- src/AComponent.hpp
+++ src/AComponent.hpp
@@ -32,8 +32,10 @@ namespace nts
             explicit AComponent(const std::string& name);
             AComponent(const AComponent &other) = delete;
             AComponent &operator=(const AComponent &other) = delete;
-            inline bool operator==(const AComponent &other) const { return this->_name == other._name; }
-            inline bool operator!=(const AComponent &other) const { return !(*this == other); }
+            void simulate(std::size_t tick) override;
+            nts::Tristate compute(std::size_t pin) override;
+            bool operator==(const IComponent &other) const override;
+            inline bool operator!=(const IComponent &other) const override  { return !(*this == other); }
             void dump() const override;
             void setLink(std::size_t pin, nts::IComponent &other, std::size_t otherPin) override;
             void setPin(std::size_t pin, nts::IComponent &other, std::size_t otherPin) override;
