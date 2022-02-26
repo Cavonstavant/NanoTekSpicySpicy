7fd30d0dde4a373f1e20c727343f4dbd24116beb
Cavonstavant <constant.vigneron@protonmail.com>
2022-02-26 15:23:22 +0100

    [+] added missing methods declaration inside Circuit class

--- src/Circuit.hpp
+++ src/Circuit.hpp
@@ -19,6 +19,14 @@ namespace nts {
             ~Circuit() override = default;
             void addComponent(nts::IComponent&);
             void removeComponent(nts::IComponent&);
+            void setLink(std::size_t, nts::IComponent &, std::size_t) override;
+            void setPin(std::size_t, nts::IComponent &, std::size_t) override;
+            inline bool operator==(const Circuit &other) const { return (this->_components == other._components);}
+            inline bool operator!=(const Circuit &other) const { return !(*this == other);}
+            [[nodiscard]] std::string getName() const override { return "";}
+            void setName(const std::string &) override {}
+            [[nodiscard]] nts::Tristate pollState(std::size_t) override;
+            void setState(std::size_t, nts::Tristate) override;
             void simulate(std::size_t) override;
             void dump() const override;
             Tristate compute(std::size_t) override;
