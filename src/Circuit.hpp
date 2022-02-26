e6423b43a235d6f7fd8266b117150f5586839f7e
Cavonstavant <constant.vigneron@protonmail.com>
2022-02-26 15:26:47 +0100

    [*] Separate inputs and outputs pin of a Component

--- src/Circuit.hpp
+++ src/Circuit.hpp
@@ -19,13 +19,17 @@ namespace nts {
             ~Circuit() override = default;
             void addComponent(nts::IComponent&);
             void removeComponent(nts::IComponent&);
+            virtual void setLink(std::size_t, nts::IComponent &, std::size_t) override = 0;
+
             void simulate(std::size_t) override;
             void dump() const override;
             Tristate compute(std::size_t) override;
         protected:
         private:
-            std::list<std::reference_wrapper<nts::IComponent>> _components;
+            std::list<std::reference_wrapper<nts::IComponent>> _inputs;
+            std::list<std::reference_wrapper<nts::IComponent>> _outputs;
             std::list<std::pair<std::size_t, Tristate>> _inputsBuffer;
+            std::list<std::pair<std::size_t, Tristate>> _outputsBuffer;
     };
 }
 
