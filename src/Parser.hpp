cfa8fe8f85c910205aa03842952173ed72f38495
Restyled.io <commits@restyled.io>
2022-03-01 23:39:01 +0000

    Restyled by clang-format

--- src/Parser.hpp
+++ src/Parser.hpp
@@ -15,21 +15,21 @@
 #include <vector>
 
 namespace nts {
-class Parser {
-public:
-    Parser();
-    ~Parser();
-    void fillCircuit(const std::string &, Circuit &);
+    class Parser {
+    public:
+        Parser();
+        ~Parser();
+        void fillCircuit(const std::string &, Circuit &);
 
-private:
-    std::vector<std::unique_ptr<nts::IComponent>> _buffer;
-    std::string trim(const std::string &str);
-    std::string trimMiddle(std::string str);
-    int findAny(std::string str, const char *chars);
-    void createLink(std::string line, Factory &factory);
-    void createChipset(std::string line, Factory &factory);
-    // Circuit getMainBoard() const;
-};
+    private:
+        std::vector<std::unique_ptr<nts::IComponent>> _buffer;
+        std::string trim(const std::string &str);
+        std::string trimMiddle(std::string str);
+        int findAny(std::string str, const char *chars);
+        void createLink(std::string line, Factory &factory);
+        void createChipset(std::string line, Factory &factory);
+        // Circuit getMainBoard() const;
+    };
 }// namespace nts
 
 #endif /* !PARSER_HPP_ */
