96a7503866664bee6d13c808d69465d476274dc8
Aurèle <aurele.nicolas@epitech.eu>
2022-03-02 00:25:45 +0100

    Component not

--- src/Components/Component4069.hpp
+++ src/Components/Component4069.hpp
@@ -9,12 +9,26 @@
 #define COMPONENT4069_HPP_
 
 #include "../AComponent.hpp"
+#include "../Exception.hpp"
+#include "../basic_gates/basic_gates.hpp"
 
 namespace nts {
     class Component4069 : public AComponent {
         public:
-            Component4069();
-            ~Component4069();
+            Component4069() = delete;
+            explicit Component4069(const std::string &name) : AComponent(name){};
+            Component4069(const Component4069 &other) = delete;
+            Component4069 &operator=(const Component4069 &other) = delete;
+            nts::Tristate compute(std::size_t pin) final {
+                if (pin == 7 || pin == 14)
+                    throw nts::Exception::InvalidPinException("Reserved pin", pin);
+                if ((pin < 1 || pin > 14))
+                    throw nts::Exception::InvalidPinException("Pin not in range", pin);
+                if (pin != 2 && pin != 4 && pin != 6 && pin != 8 && pin != 10 && pin != 12)
+                    throw nts::Exception::InvalidPinException("Trying to compute the state of an input pin", pin);
+                return nts::notGate(this->getState(pin - 1));
+            }
+            ~Component4069() override = default;
 
         protected:
         private:
