731ce585aa83395a2d27cec3e7881e8287abfd89
Aurèle <aurele.nicolas@epitech.eu>
2022-03-01 19:11:35 +0100

    parsing without circuit
    Co-authored-by: Cavonstavant <Cavonstavant@users.noreply.github.com>

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -29,13 +29,31 @@ static inline void trimLine(std::string &line) {
                line.end());
 }
 
+size_t strtosize(const std::string &str) {
+    size_t size = 0;
+    std::stringstream ss;
+    ss << str;
+    ss >> size;
+    return size;
+}
+
+// std::reference_wrapper<std::unique_ptr<nts::IComponent>> &nts::Parser::findComponent(std::string name)
+// {
+//     for (std::reference_wrapper<std::unique_ptr<IComponent>> &component: _mainBoard) {
+//         if (component.get().get()->getName() == name) {
+//             return component;
+//         }
+//     }
+//     return nullptr;
+// }
+
 /**
  * @brief parse the file given with the path and fills the circuit
  * @param file
  * @param circuit
  * @throw nts::ParserError
  */
-void nts::Parser::createLink(std::string line, Circuit &mainBoard, Factory &factory)
+void nts::Parser::createLink(std::string line, Factory &factory)
 {
     std::string component1;
     std::string component2;
@@ -56,10 +74,27 @@ void nts::Parser::createLink(std::string line, Circuit &mainBoard, Factory &fact
     }
     if (component1 == component2 && pin1 == pin2)
         throw nts::Exception::VeryStupidUserError("You can't link a component's pin to itself");
+
+    try {
+        // _mainBoard[component1].get().setLink(strtosize(pin1), _mainBoard[component2].get(), strtosize(pin2));
+    } catch (std::exception &e) {
+        throw nts::Exception::InvalidSyntaxException("Invalid syntax");
+    }
+
+
     // mainBoard[component1]->setLink(pin1, mainBoard[component2], pin2);
+    // _mainBoard[component1]->setLink(pin1, _mainBoard[component2], pin2);
+
+    // std::reference_wrapper<std::unique_ptr<nts::IComponent>> &component1Ref = findComponent(component1);
+    // for (std::reference_wrapper<std::unique_ptr<IComponent>> &component: _mainBoard) {
+    //     if (component.get().get()->getName() == component1) {
+    //         component.get().get()->setLink(strtosize(pin1), component1Ref.get().get(), strtosize(pin2));
+    //         return;
+    //     }
+    // }
 }
 
-void nts::Parser::createChipset(std::string line, Circuit &mainBoard, Factory &factory)
+void nts::Parser::createChipset(std::string line, Factory &factory)
 {
     std::string type;
     std::string name;
@@ -71,13 +106,14 @@ void nts::Parser::createChipset(std::string line, Circuit &mainBoard, Factory &f
         throw nts::Exception::InvalidReadException("Invalid syntax");
     }
     try {
-        mainBoard.addComponent(factory.createComponent(type));
+        // _mainBoard.emplace(std::make_pair(name, factory.createComponent(type)));
+        // mainBoard.addComponent(factory.createComponent(type));
     } catch (std::exception) {
         throw nts::Exception::InvalidComponentNameException("Invalid component name");
     }
 }
 
-void nts::Parser::fillCircuit(const std::string& file, Circuit &circuit)
+void nts::Parser::fillMainBoard(const std::string& file)
 {
     std::ifstream input(file);
     if (!input.is_open())
@@ -97,12 +133,39 @@ void nts::Parser::fillCircuit(const std::string& file, Circuit &circuit)
             isLink = false;
             continue;
         } else if (isLink == false) {
-            createChipset(line, circuit, factory);
+            createChipset(line, factory);
         } else {
-            createLink(line, circuit, factory);
+            createLink(line, factory);
         }
     }
 }
+
+// void nts::Parser::fillCircuit(const std::string& file, Circuit &circuit)
+// {
+//     std::ifstream input(file);
+//     if (!input.is_open())
+//         throw std::runtime_error("Can't open file");
+//     std::string line;
+//     bool isLink = false;
+//     Factory factory;
+
+//     while (std::getline(input, line)) {
+//         line = trim(line);
+//         if (line.empty() || line[0] == '#')
+//             continue;
+//         if (line.rfind(".links:", 0) == 0) {
+//             isLink = true;
+//             continue;
+//         } else if (line.rfind(".chipsets:", 0) == 0) {
+//             isLink = false;
+//             continue;
+//         } else if (isLink == false) {
+//             createChipset(line, circuit, factory);
+//         } else {
+//             createLink(line, circuit, factory);
+//         }
+//     }
+// }
 
 int nts::Parser::findAny(std::string str, const char *chars)
 {
