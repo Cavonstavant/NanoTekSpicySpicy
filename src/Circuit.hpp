edded7a756e77c6d4e433564dfc5783607f75808
Restyled.io <commits@restyled.io>
2022-03-01 20:58:26 +0000

    Restyled by astyle

--- src/Circuit.hpp
+++ src/Circuit.hpp
@@ -18,21 +18,21 @@
 namespace nts
 {
 
-    class Circuit {
-        public:
-            Circuit() = default;
-            ~Circuit() = default;
+class Circuit {
+public:
+    Circuit() = default;
+    ~Circuit() = default;
 
-            void addComponent(std::string const &name, std::reference_wrapper<IComponent> component);
-            void addInputComponent(std::string const &name, std::reference_wrapper<IComponent> inputComponent);
-            void addOutputComponent(std::string const &name, std::reference_wrapper<IComponent> outputComponent);
-            void dump() const;
-            void simulate(std::string const &inputs, std::string const &outputs);
-        private:
-            std::unordered_map<std::string, std::reference_wrapper<IComponent>> _internalComponents;
-            std::unordered_map<std::string, std::reference_wrapper<IComponent>> _inputComponents;
-            std::unordered_map<std::string, std::reference_wrapper<IComponent>> _outputComponents;
-    };
+    void addComponent(std::string const &name, std::reference_wrapper<IComponent> component);
+    void addInputComponent(std::string const &name, std::reference_wrapper<IComponent> inputComponent);
+    void addOutputComponent(std::string const &name, std::reference_wrapper<IComponent> outputComponent);
+    void dump() const;
+    void simulate(std::string const &inputs, std::string const &outputs);
+private:
+    std::unordered_map<std::string, std::reference_wrapper<IComponent>> _internalComponents;
+    std::unordered_map<std::string, std::reference_wrapper<IComponent>> _inputComponents;
+    std::unordered_map<std::string, std::reference_wrapper<IComponent>> _outputComponents;
+};
 
 }
 
