1a4a36ccb214707ea8337096dbb48bd5cd7319dd
Restyled.io <commits@restyled.io>
2022-03-01 23:45:21 +0000

    Restyled by clang-format

--- src/Components/Component4011.hpp
+++ src/Components/Component4011.hpp
@@ -14,34 +14,34 @@
 
 
 namespace nts {
-class Component4011 : public AComponent {
-public:
-    Component4011() = delete;
-    explicit Component4011(const std::string &name) : AComponent(name) {};
-    Component4011(const Component4011 &other) = delete;
-    Component4011 &operator=(const Component4011 &other) = delete;
-    nts::Tristate compute(std::size_t pin) final {
-        if (pin == 7 || pin == 14)
-            throw nts::Exception::InvalidPinException("Reserved pin", pin);
-        if ((pin < 1 || pin > 14))
-            throw nts::Exception::InvalidPinException("Pin not in range", pin);
-        if (pin != 3 && pin != 4 && pin != 10 && pin != 11)
-            throw nts::Exception::InvalidPinException("Trying to compute the state of an input pin", pin);
-        switch (pin) {
-        case 3:
-            return (nts::nandGate(this->getState(1), this->getState(2)));
-        case 4:
-            return (nts::nandGate(this->getState(5), this->getState(6)));
-        case 10:
-            return (nts::nandGate(this->getState(9), this->getState(8)));
+    class Component4011 : public AComponent {
+        public:
+        Component4011() = delete;
+        explicit Component4011(const std::string &name) : AComponent(name){};
+        Component4011(const Component4011 &other) = delete;
+        Component4011 &operator=(const Component4011 &other) = delete;
+        nts::Tristate compute(std::size_t pin) final {
+            if (pin == 7 || pin == 14)
+                throw nts::Exception::InvalidPinException("Reserved pin", pin);
+            if ((pin < 1 || pin > 14))
+                throw nts::Exception::InvalidPinException("Pin not in range", pin);
+            if (pin != 3 && pin != 4 && pin != 10 && pin != 11)
+                throw nts::Exception::InvalidPinException("Trying to compute the state of an input pin", pin);
+            switch (pin) {
+                case 3:
+                    return (nts::nandGate(this->getState(1), this->getState(2)));
+                case 4:
+                    return (nts::nandGate(this->getState(5), this->getState(6)));
+                case 10:
+                    return (nts::nandGate(this->getState(9), this->getState(8)));
+            }
+            return (nts::nandGate(this->getState(13), this->getState(12)));
         }
-        return (nts::nandGate(this->getState(13), this->getState(12)));
-    }
-    ~Component4011() override = default;
+        ~Component4011() override = default;
 
-protected:
-private:
-};
-};
+        protected:
+        private:
+    };
+};// namespace nts
 
 #endif /* !COMPONENT4011_HPP_ */
